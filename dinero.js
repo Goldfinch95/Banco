3cc4ad09c9716849f5b7552bf6cd3156c9a9c605
Goldfinch95 <facu_vila@hotmail.com>
2022-06-08 13:37:39 -0300

    base

---
+++ dinero.js
@@ -0,0 +1,39 @@
+/*Desarrollar un programa que le permita al usuario conocer cuanto dinero le queda (saldo) luego de cobrar por los trabajos realizados y gastar dinero por las compras efectuadas. Para lograr esto el programa debe presentar un menú como el siguiente:
+1.	Un botonIngresos por trabajos realizados (boton:azul)
+2.Div con tres input (tipo de ingreso, descripción,monto)
+3.	Egresos por compras realizadas (boton: rojo)
+4.div con tres input (tipo de ingreso, descripción, monto)
+5.	listo (agregar sonido de herstone (troll))
+Cada ingreso percibido por el usuario, se acumulará y su saldo crecerá. 
+Cada egreso que el usuario genere, se restará del saldo, el cual decrecerá. 
+Luego de realizada cada operación deberá mostrarse en pantalla el valor actual del saldo disponible.
+Cuando el usuario elija la opción salir del programa se deberá visualizar:
+1.	La cantidad de operciones realizadas en concepto de ingresos.
+2.	La cantidad de operaciones realizadas en concepto de egresos.
+3.	La cantidad de pesos percibidos considerando todos los ingresos.
+4.	La cantidad de pesos erogados (gastados) considerando todos los egresos.
+5.	El ingreso que más dinero representó y que lugar ocupó (1ero, 2do, 3ero etc)
+6.	El egreso que más dinero representó y que lugar ocupó (1ero, 2do, 3ero etc)
+7.	El promedio de pesos por ingresos.
+8.	El promedio de pesos por egresos. 
+9.	El saldo (dinero que le quedó al usuario). Si el saldo del usuario es mayor o igual a cero se deberá mostrar en verde. Si es menor que cero en rojo.*/
+
+const botonIngresar = document.getElementById("submit");
+const resultado = document.getElementById ("resultado");
+
+let ingresos = [];
+
+const agregarIngresos = ()=> {
+    const dineroGanado = document.getElementById ("ingreso");
+    let totalIngresos = 0
+    ingresos.push (Number(dineroGanado.value));
+    for( let i= 0; i < ingresos.length; i++){
+        totalIngresos =  totalIngresos + ingresos[i];
+    }
+    resultado.innerHTML = `Resultado: ${totalIngresos}`;
+}
+
+botonIngresar.addEventListener ("click", agregarIngresos);
+
+
+
